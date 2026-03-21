424ff0255b71980f69122ff234ca3d8184635cb8
Olibot1107 <ojdawson1107@gmail.com>
2026-03-21 20:38:06 +0000

    e

---
+++ admin/view.blade.php
@@ -0,0 +1,12 @@
+<div class="row">
+    <div class="col-xs-12">
+        <div class="box">
+            <div class="box-header with-border">
+                <h3 class="box-title">Node Status</h3>
+            </div>
+            <div class="box-body">
+                <p>This extension has no admin-configurable settings. View the Nodes tab from the dashboard to monitor your Wings nodes.</p>
+            </div>
+        </div>
+    </div>
+</div>
